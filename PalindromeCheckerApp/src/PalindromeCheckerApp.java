74e56627dcf41a69920b05ebf130d16ea67d218a
Shivvy-alt <sa9574@srmist.edu.in>
2026-02-26 15:01:12 +0530

    Add palindrome result feature

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++
@@ -1,8 +0,0 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker Managemnt System");
-        System.out.println("Version: 1.0");
-        System.out.println("System iNitailised Successsfully");
-    }
-    }
-
---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class PalindromeCheckerApp {
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args){
+        String input= "madam";
+        boolean isPalindrome= true;
+        for(int i=0;i<input.length()/2;i++){
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)){
+                isPalindrome= false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println(input + " is a palindrome");
+        }
+        else{
+            System.out.println(input + " is not a palindrome");
+        }
+    }
+}
